687dbdafe5ceb921197a0c54a9a1183546cb9357
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-13 12:38:22 -0600

    added copy constructor for MediaProduct as well as getters/setters

--- Homework_3/src/edu/mu/MediaProduct.java
+++ Homework_3/src/edu/mu/MediaProduct.java
@@ -1,5 +1,49 @@
 package edu.mu;
 
 public class MediaProduct {
+	protected String title;
+	protected double price;
+	protected int year;
+	protected Genre genre;
+	
+	public MediaProduct(MediaProduct mediaProduct) {
+		this.title = mediaProduct.title;
+		this.price = mediaProduct.price;
+		this.year = mediaProduct.year;
+		this.genre = mediaProduct.genre;
+	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
+	
 }
