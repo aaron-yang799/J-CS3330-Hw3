52b1ef65d9d3054430987f046f3c3d19095bbf30
zachrsnyder <zachrsnyder2004@gmail.com>
2024-02-13 12:56:16 -0600

    Added singleton design pattern to StockerManagerSingleton with static (single instance) attribute of file path and a products array list. Started work on reading from the csv.

--- Homework_3/src/edu/mu/StockerManagerSingleton.java
+++ Homework_3/src/edu/mu/StockerManagerSingleton.java
@@ -1,5 +1,45 @@
 package edu.mu;
 
-public class StockerManagerSingleton {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class StockerManagerSingleton {
+	
+	private static StockerManagerSingleton instance = null;
+	private final static String filePath = "files/inventory.csv";
+	private static ArrayList<MediaProduct> products;
+	
+	private StockerManagerSingleton() {
+		products = new ArrayList<MediaProduct>();
+	}
+	
+	public static StockerManagerSingleton getInstance() {
+		if(instance == null) {
+			instance = new StockerManagerSingleton();
+		}
+		return instance;
+	}
+	
+	public boolean initializeStock() {
+		try{
+			FileInputStream inputStream = new FileInputStream(filePath);
+		
+			Scanner scanner = new Scanner(inputStream);
+			scanner.nextLine();
+			while(scanner.hasNextLine()) {
+				
+			}
+			scanner.close();
+			inputStream.close();
+			return true;
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+			
+	}
 }
+
