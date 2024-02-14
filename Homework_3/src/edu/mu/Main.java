e3b25edbbf93bbf75997b447ddd4d614b6b0b7ab
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-14 14:21:16 -0600

    added colored text for return values of certain methods

--- Homework_3/src/edu/mu/Main.java
+++ Homework_3/src/edu/mu/Main.java
@@ -6,12 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+        String RESET = "\u001B[0m";
+		String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+		
 		StockerManagerSingleton instance = StockerManagerSingleton.getInstance();
 		System.out.println(instance.initializeStock());
 		
 		ArrayList<MediaProduct> listOfProducts = StockerManagerSingleton.getProducts();
 		
-		System.out.println("Result of savStock: " + instance.saveStock());
+		System.out.println("Result of savStock: " + GREEN + instance.saveStock() + RESET);
 		
 		System.out.println("\nList of products including the class: ");
 		
@@ -24,9 +28,9 @@ public class Main {
 		
 		System.out.println("\n\n");
 		
-		System.out.println("Result of removeItem used on the second item: " + instance.removeItem(listOfProducts.get(1)));
+		System.out.println("Result of removeItem used on the second item: " + GREEN + instance.removeItem(listOfProducts.get(1)) + RESET);
 				
-		System.out.println("\nResult of updateItemPrice used on Baby Shark (new price: 23.13, previous price: 10.99): " +instance.updateItemPrice(listOfProducts.get(2), 23.13));
+		System.out.println("\nResult of updateItemPrice used on Baby Shark (new price: 23.13, previous price: 10.99): " + GREEN + instance.updateItemPrice(listOfProducts.get(2), 23.13) + RESET);
 		
 		System.out.println("\nList of products after updateItemPrice and removeItem: \n");
 		
@@ -40,7 +44,7 @@ public class Main {
 		
 		MediaProduct temp = new CDRecordProduct(new MediaProduct("Test Track", 10.99, 2021, Genre.POP));
 		
-		System.out.println("Result of addItem (adding: Test Track, CDRecordProduct): " + instance.addItem(temp));
+		System.out.println("Result of addItem (adding: Test Track, CDRecordProduct): " + GREEN + instance.addItem(temp) + RESET);
 		
 		System.out.println("\nList of products after adding Test Track: \n");
 		
@@ -60,7 +64,7 @@ public class Main {
 			System.out.println(vpr.get(i));
 			System.out.println((vpr.get(i)).getClass());
 		}
-		System.out.println("\nTotal number of CDRecords: " + vpr.size());
+		System.out.println("\nTotal number of CDRecords: " + YELLOW + vpr.size() + RESET);
 		
 		
 		System.out.println("\n\n");
@@ -74,7 +78,7 @@ public class Main {
 			System.out.println(cdp.get(i));
 			System.out.println((cdp.get(i)).getClass());
 		}
-		System.out.println("\nTotal number of CDRecords: " + cdp.size());
+		System.out.println("\nTotal number of CDRecords: " + YELLOW + cdp.size() + RESET);
 		
 		
 		System.out.println("\n\n");
@@ -87,10 +91,10 @@ public class Main {
 			System.out.println(trp.get(i));
 			System.out.println((trp.get(i)).getClass());
 		}
-		System.out.println("\nTotal number of TapeRecords: " + trp.size());
+		System.out.println("\nTotal number of TapeRecords: " + YELLOW + trp.size() + RESET);
 
 		
-		System.out.println("\n\nTapes below $16:");
+		System.out.println("\n\nTapes below $16: \n");
 		ArrayList<MediaProduct> budget = new ArrayList<>();
 		budget = instance.getMediaProductBelowPrice(16);
 		for(int i = 0; i < budget.size(); i++) {	
