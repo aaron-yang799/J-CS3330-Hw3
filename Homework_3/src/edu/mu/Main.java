1cb0c8ba1b07d9fea9d4843674684e9a9db79a06
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-14 14:12:04 -0600

    made print lines for testing organized and readable

--- Homework_3/src/edu/mu/Main.java
+++ Homework_3/src/edu/mu/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		System.out.println("Yippee!");
 		StockerManagerSingleton instance = StockerManagerSingleton.getInstance();
 		System.out.println(instance.initializeStock());
 		
 		ArrayList<MediaProduct> listOfProducts = StockerManagerSingleton.getProducts();
 		
-		System.out.println("List of products before removeItem: ");
+		System.out.println("Result of savStock: " + instance.saveStock());
+		
+		System.out.println("\nList of products including the class: ");
 		
 		for(int i = 0; i < listOfProducts.size(); i++)
 		{
@@ -22,99 +22,80 @@ public class Main {
 
 		}
 		
-		instance.saveStock();
+		System.out.println("\n\n");
 		
-		instance.removeItem(listOfProducts.get(1));
+		System.out.println("Result of removeItem used on the second item: " + instance.removeItem(listOfProducts.get(1)));
+				
+		System.out.println("\nResult of updateItemPrice used on Baby Shark (new price: 23.13, previous price: 10.99): " +instance.updateItemPrice(listOfProducts.get(2), 23.13));
 		
-		System.out.println("List of products after removeItem: \n");
-
-		for(int i = 0; i < listOfProducts.size(); i++)
-		{
-			System.out.println(listOfProducts.get(i));
-			System.out.println(listOfProducts.get(i).getClass());
-		}
-		
-		System.out.println("\n\n\n\n\n\n");
-		
-		
-		System.out.println("\nList of products after updateItemPrice: \n");
-
-		instance.updateItemPrice(listOfProducts.get(2), 23.13);
+		System.out.println("\nList of products after updateItemPrice and removeItem: \n");
 		
 		for(int i = 0; i < listOfProducts.size(); i++)
 		{
 			System.out.println(listOfProducts.get(i));
-			System.out.println(listOfProducts.get(i).getClass());
+//			System.out.println(listOfProducts.get(i).getClass());
 		}
 		
-		System.out.println("\n\n\n\n\n\n");
+		System.out.println("\n\n");
 		
-		MediaProduct temp = new CDRecordProduct(new MediaProduct("Monkey Track", 10.99, 2021, Genre.POP));
-
-		System.out.println("\nList of products after addItem: \n");
+		MediaProduct temp = new CDRecordProduct(new MediaProduct("Test Track", 10.99, 2021, Genre.POP));
 		
-		instance.addItem(temp);
+		System.out.println("Result of addItem (adding: Test Track, CDRecordProduct): " + instance.addItem(temp));
+		
+		System.out.println("\nList of products after adding Test Track: \n");
 		
 		for(int i = 0; i < listOfProducts.size(); i++)
 		{
 			System.out.println(listOfProducts.get(i));
-			System.out.println(listOfProducts.get(i).getClass());
+//			System.out.println(listOfProducts.get(i).getClass());
 		}
 		
-		System.out.println("\n\n\n\n\n\n");
+		System.out.println("\n\n");
 		ArrayList<VinylRecordProduct> vpr = new ArrayList<VinylRecordProduct>();
 		System.out.println("\nList of VinylRecords only: \n");
 		vpr = instance.getVinylRecordList(listOfProducts);
 		
 		for(int i = 0; i < vpr.size(); i++)
-		{
-			
-			
+		{			
 			System.out.println(vpr.get(i));
 			System.out.println((vpr.get(i)).getClass());
-//			System.out.println("\n");
 		}
-		System.out.println("Total number of CDRecords:" + vpr.size());
+		System.out.println("\nTotal number of CDRecords: " + vpr.size());
 		
 		
-		System.out.println("\n\n\n\n\n\n");
+		System.out.println("\n\n");
+		
 		ArrayList<CDRecordProduct> cdp = new ArrayList<CDRecordProduct>();
 		System.out.println("\nList of CDRecords only: \n");
 		cdp = instance.getCDRecordsList(listOfProducts);
 		
 		for(int i = 0; i < cdp.size(); i++)
-		{
-			
-			
+		{	
 			System.out.println(cdp.get(i));
 			System.out.println((cdp.get(i)).getClass());
-//			System.out.println("\n");
 		}
-		System.out.println("Total number of CDRecords:" + cdp.size());
+		System.out.println("\nTotal number of CDRecords: " + cdp.size());
 		
 		
-		System.out.println("\n\n\n\n\n\n");
+		System.out.println("\n\n");
 		ArrayList<TapeRecordProduct> trp = new ArrayList<TapeRecordProduct>();
 		System.out.println("\nList of TapeRecords only: \n");
 		trp = instance.getTapeRecordList(listOfProducts);
 		
 		for(int i = 0; i < trp.size(); i++)
-		{
-			
-			
+		{	
 			System.out.println(trp.get(i));
 			System.out.println((trp.get(i)).getClass());
-//			System.out.println("\n");
 		}
-		System.out.println("Total number of TapeRecords:" + trp.size());
+		System.out.println("\nTotal number of TapeRecords: " + trp.size());
 
 		
 		System.out.println("\n\nTapes below $16:");
 		ArrayList<MediaProduct> budget = new ArrayList<>();
 		budget = instance.getMediaProductBelowPrice(16);
-		for(int i = 0; i < budget.size(); i++)
+		for(int i = 0; i < budget.size(); i++) {	
 			System.out.println(budget.get(i));
-
+		}
 		
 	}
 	
